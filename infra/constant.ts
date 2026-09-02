c0a4a0985c724627a9ebe396f6116493d49f0599
siddharth0808 <55535274+siddharth0808@users.noreply.github.com>
2026-09-02 18:06:55 +0530

    [fix] Prod: cors issue

--- infra/constant.ts
+++ infra/constant.ts
@@ -1,5 +1,6 @@
 export const APP_NAME = 'goods-wise';
 export const ALLOW_ORIGINS = [
     "http://localhost:5173",
-    "https://qa.dudrj9bmh1c22.amplifyapp.com"
+    "https://qa.dudrj9bmh1c22.amplifyapp.com",
+    "https://prod.dzonbxulrckkr.amplifyapp.com"
 ]
