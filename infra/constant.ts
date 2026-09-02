c8d3081b608601857142ededd6063dcd0fcb6553
siddharth0808 <55535274+siddharth0808@users.noreply.github.com>
2026-09-02 17:46:39 +0530

    [fix] deploy: update workflow

--- infra/constant.ts
+++ infra/constant.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = 'inventory-flow';
+export const APP_NAME = 'goods-wise';
 export const ALLOW_ORIGINS = [
     "http://localhost:5173",
     "https://qa.dudrj9bmh1c22.amplifyapp.com"
